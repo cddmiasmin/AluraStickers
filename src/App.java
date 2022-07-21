5d197e02fa442d75c65f567cbb61cce55456edf3
Iasmin Dias <iasmin.dias87@gmail.com>
2022-07-21 19:42:29 -0300

    :woman_technologist: Consumindo API IMDB

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // fazer uma conexão HTTP e buscar os top 250 filmes
+        // String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+        String url = "https://alura-filmes.herokuapp.com/conteudos";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // extrair só os dados que interessam (titulo, poster, classificação)
+        var jsonParser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = jsonParser.parse(body);
+        System.out.println(listaDeFilmes.size());
+
+        // exibir e manipular os dados 
     }
 }
